d6ff2dedb08450622ca4b949ff985caf8016b5ae
Cole Charles <38669367+colegcharles@users.noreply.github.com>
2019-05-05 12:03:33 -0400

    recent 5/5

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -52,6 +52,7 @@
             this.label3 = new System.Windows.Forms.Label();
             this.decryptButton = new System.Windows.Forms.Button();
             this.resetButton = new System.Windows.Forms.Button();
+            this.buttonHast = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
@@ -71,60 +72,54 @@
             // 
             this.pictureBox1.Image = global::securityProject.Properties.Resources.lena_gray1;
             this.pictureBox1.InitialImage = null;
-            this.pictureBox1.Location = new System.Drawing.Point(18, 10);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Location = new System.Drawing.Point(27, 15);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(107, 112);
+            this.pictureBox1.Size = new System.Drawing.Size(160, 172);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(218, 10);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox2.Location = new System.Drawing.Point(327, 15);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(124, 128);
+            this.pictureBox2.Size = new System.Drawing.Size(186, 197);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox2.TabIndex = 1;
             this.pictureBox2.TabStop = false;
             // 
             // pictureBox3
             // 
-            this.pictureBox3.Location = new System.Drawing.Point(346, 10);
-            this.pictureBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox3.Location = new System.Drawing.Point(519, 15);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(124, 128);
+            this.pictureBox3.Size = new System.Drawing.Size(186, 197);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox3.TabIndex = 3;
             this.pictureBox3.TabStop = false;
             // 
             // pictureBox4
             // 
-            this.pictureBox4.Location = new System.Drawing.Point(476, 10);
-            this.pictureBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox4.Location = new System.Drawing.Point(714, 15);
             this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(124, 128);
+            this.pictureBox4.Size = new System.Drawing.Size(186, 197);
             this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox4.TabIndex = 4;
             this.pictureBox4.TabStop = false;
             // 
             // pictureBox5
             // 
-            this.pictureBox5.Location = new System.Drawing.Point(604, 10);
-            this.pictureBox5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox5.Location = new System.Drawing.Point(906, 15);
             this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(124, 128);
+            this.pictureBox5.Size = new System.Drawing.Size(186, 197);
             this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox5.TabIndex = 5;
             this.pictureBox5.TabStop = false;
             // 
             // pictureBox6
             // 
-            this.pictureBox6.Location = new System.Drawing.Point(18, 150);
-            this.pictureBox6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox6.Location = new System.Drawing.Point(27, 231);
             this.pictureBox6.Name = "pictureBox6";
-            this.pictureBox6.Size = new System.Drawing.Size(107, 112);
+            this.pictureBox6.Size = new System.Drawing.Size(160, 172);
             this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox6.TabIndex = 6;
             this.pictureBox6.TabStop = false;
@@ -134,20 +129,17 @@
             this.groupBox1.Controls.Add(this.buttonGenerateShares);
             this.groupBox1.Controls.Add(this.radioButtonShare2);
             this.groupBox1.Controls.Add(this.radioButtonShare4);
-            this.groupBox1.Location = new System.Drawing.Point(130, 21);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Location = new System.Drawing.Point(195, 32);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.groupBox1.Size = new System.Drawing.Size(82, 108);
+            this.groupBox1.Size = new System.Drawing.Size(123, 166);
             this.groupBox1.TabIndex = 8;
             this.groupBox1.TabStop = false;
             // 
             // buttonGenerateShares
             // 
-            this.buttonGenerateShares.Location = new System.Drawing.Point(4, 63);
-            this.buttonGenerateShares.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonGenerateShares.Location = new System.Drawing.Point(6, 97);
             this.buttonGenerateShares.Name = "buttonGenerateShares";
-            this.buttonGenerateShares.Size = new System.Drawing.Size(64, 37);
+            this.buttonGenerateShares.Size = new System.Drawing.Size(96, 57);
             this.buttonGenerateShares.TabIndex = 11;
             this.buttonGenerateShares.Text = "Generate Shares";
             this.buttonGenerateShares.UseVisualStyleBackColor = true;
@@ -156,10 +148,9 @@
             // radioButtonShare2
             // 
             this.radioButtonShare2.AutoSize = true;
-            this.radioButtonShare2.Location = new System.Drawing.Point(4, 6);
-            this.radioButtonShare2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.radioButtonShare2.Location = new System.Drawing.Point(6, 9);
             this.radioButtonShare2.Name = "radioButtonShare2";
-            this.radioButtonShare2.Size = new System.Drawing.Size(67, 17);
+            this.radioButtonShare2.Size = new System.Drawing.Size(98, 24);
             this.radioButtonShare2.TabIndex = 12;
             this.radioButtonShare2.TabStop = true;
             this.radioButtonShare2.Text = "2 Shares";
@@ -168,10 +159,9 @@
             // radioButtonShare4
             // 
             this.radioButtonShare4.AutoSize = true;
-            this.radioButtonShare4.Location = new System.Drawing.Point(4, 28);
-            this.radioButtonShare4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.radioButtonShare4.Location = new System.Drawing.Point(6, 43);
             this.radioButtonShare4.Name = "radioButtonShare4";
-            this.radioButtonShare4.Size = new System.Drawing.Size(67, 17);
+            this.radioButtonShare4.Size = new System.Drawing.Size(98, 24);
             this.radioButtonShare4.TabIndex = 13;
             this.radioButtonShare4.TabStop = true;
             this.radioButtonShare4.Text = "4 Shares";
@@ -181,11 +171,9 @@
             // 
             this.groupBox2.Controls.Add(this.radioButtonEBC);
             this.groupBox2.Controls.Add(this.radioButtonCBC);
-            this.groupBox2.Location = new System.Drawing.Point(548, 142);
-            this.groupBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox2.Location = new System.Drawing.Point(822, 218);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.groupBox2.Size = new System.Drawing.Size(52, 59);
+            this.groupBox2.Size = new System.Drawing.Size(78, 91);
             this.groupBox2.TabIndex = 9;
             this.groupBox2.TabStop = false;
             // 
@@ -193,10 +181,9 @@
             // 
             this.radioButtonEBC.AutoSize = true;
             this.radioButtonEBC.Enabled = false;
-            this.radioButtonEBC.Location = new System.Drawing.Point(2, 37);
-            this.radioButtonEBC.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.radioButtonEBC.Location = new System.Drawing.Point(3, 57);
             this.radioButtonEBC.Name = "radioButtonEBC";
-            this.radioButtonEBC.Size = new System.Drawing.Size(46, 17);
+            this.radioButtonEBC.Size = new System.Drawing.Size(67, 24);
             this.radioButtonEBC.TabIndex = 1;
             this.radioButtonEBC.Text = "EBC";
             this.radioButtonEBC.UseVisualStyleBackColor = true;
@@ -206,10 +193,9 @@
             this.radioButtonCBC.AutoSize = true;
             this.radioButtonCBC.Checked = true;
             this.radioButtonCBC.Enabled = false;
-            this.radioButtonCBC.Location = new System.Drawing.Point(2, 15);
-            this.radioButtonCBC.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.radioButtonCBC.Location = new System.Drawing.Point(3, 23);
             this.radioButtonCBC.Name = "radioButtonCBC";
-            this.radioButtonCBC.Size = new System.Drawing.Size(46, 17);
+            this.radioButtonCBC.Size = new System.Drawing.Size(67, 24);
             this.radioButtonCBC.TabIndex = 0;
             this.radioButtonCBC.TabStop = true;
             this.radioButtonCBC.Text = "CBC";
@@ -218,10 +204,9 @@
             // buttonEncrypt
             // 
             this.buttonEncrypt.Enabled = false;
-            this.buttonEncrypt.Location = new System.Drawing.Point(617, 159);
-            this.buttonEncrypt.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonEncrypt.Location = new System.Drawing.Point(926, 245);
             this.buttonEncrypt.Name = "buttonEncrypt";
-            this.buttonEncrypt.Size = new System.Drawing.Size(84, 27);
+            this.buttonEncrypt.Size = new System.Drawing.Size(126, 42);
             this.buttonEncrypt.TabIndex = 10;
             this.buttonEncrypt.Text = "Encrypt";
             this.buttonEncrypt.UseVisualStyleBackColor = true;
@@ -229,50 +214,45 @@
             // 
             // pictureBox7
             // 
-            this.pictureBox7.Location = new System.Drawing.Point(218, 212);
-            this.pictureBox7.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox7.Location = new System.Drawing.Point(327, 326);
             this.pictureBox7.Name = "pictureBox7";
-            this.pictureBox7.Size = new System.Drawing.Size(124, 128);
+            this.pictureBox7.Size = new System.Drawing.Size(186, 197);
             this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox7.TabIndex = 11;
             this.pictureBox7.TabStop = false;
             // 
             // pictureBox8
             // 
-            this.pictureBox8.Location = new System.Drawing.Point(346, 212);
-            this.pictureBox8.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox8.Location = new System.Drawing.Point(519, 326);
             this.pictureBox8.Name = "pictureBox8";
-            this.pictureBox8.Size = new System.Drawing.Size(124, 128);
+            this.pictureBox8.Size = new System.Drawing.Size(186, 197);
             this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox8.TabIndex = 12;
             this.pictureBox8.TabStop = false;
             // 
             // pictureBox9
             // 
-            this.pictureBox9.Location = new System.Drawing.Point(476, 212);
-            this.pictureBox9.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox9.Location = new System.Drawing.Point(714, 326);
             this.pictureBox9.Name = "pictureBox9";
-            this.pictureBox9.Size = new System.Drawing.Size(124, 128);
+            this.pictureBox9.Size = new System.Drawing.Size(186, 197);
             this.pictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox9.TabIndex = 13;
             this.pictureBox9.TabStop = false;
             // 
             // pictureBox10
             // 
-            this.pictureBox10.Location = new System.Drawing.Point(604, 212);
-            this.pictureBox10.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox10.Location = new System.Drawing.Point(906, 326);
             this.pictureBox10.Name = "pictureBox10";
-            this.pictureBox10.Size = new System.Drawing.Size(124, 128);
+            this.pictureBox10.Size = new System.Drawing.Size(186, 197);
             this.pictureBox10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox10.TabIndex = 14;
             this.pictureBox10.TabStop = false;
             // 
             // pictureBox11
             // 
-            this.pictureBox11.Location = new System.Drawing.Point(18, 288);
-            this.pictureBox11.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox11.Location = new System.Drawing.Point(27, 443);
             this.pictureBox11.Name = "pictureBox11";
-            this.pictureBox11.Size = new System.Drawing.Size(107, 112);
+            this.pictureBox11.Size = new System.Drawing.Size(160, 172);
             this.pictureBox11.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox11.TabIndex = 15;
             this.pictureBox11.TabStop = false;
@@ -280,20 +260,18 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(28, 124);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(42, 191);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.Size = new System.Drawing.Size(115, 20);
             this.label1.TabIndex = 16;
             this.label1.Text = "plaintext image";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(28, 264);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(42, 406);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.Size = new System.Drawing.Size(130, 20);
             this.label2.TabIndex = 17;
             this.label2.Text = "Encrypted Image";
             this.label2.Visible = false;
@@ -301,10 +279,9 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(28, 402);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(42, 618);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(88, 13);
+            this.label3.Size = new System.Drawing.Size(131, 20);
             this.label3.TabIndex = 18;
             this.label3.Text = "Decrypted Image";
             this.label3.Visible = false;
@@ -312,10 +289,9 @@
             // decryptButton
             // 
             this.decryptButton.Enabled = false;
-            this.decryptButton.Location = new System.Drawing.Point(617, 373);
-            this.decryptButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.decryptButton.Location = new System.Drawing.Point(926, 574);
             this.decryptButton.Name = "decryptButton";
-            this.decryptButton.Size = new System.Drawing.Size(84, 27);
+            this.decryptButton.Size = new System.Drawing.Size(126, 42);
             this.decryptButton.TabIndex = 19;
             this.decryptButton.Text = "Decrypt";
             this.decryptButton.UseVisualStyleBackColor = true;
@@ -323,19 +299,28 @@
             // 
             // resetButton
             // 
-            this.resetButton.Location = new System.Drawing.Point(529, 373);
-            this.resetButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.resetButton.Location = new System.Drawing.Point(794, 574);
             this.resetButton.Name = "resetButton";
-            this.resetButton.Size = new System.Drawing.Size(84, 27);
+            this.resetButton.Size = new System.Drawing.Size(126, 42);
             this.resetButton.TabIndex = 0;
             this.resetButton.Text = "Reset";
             this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
             // 
+            // buttonHast
+            // 
+            this.buttonHast.Location = new System.Drawing.Point(631, 573);
+            this.buttonHast.Name = "buttonHast";
+            this.buttonHast.Size = new System.Drawing.Size(157, 42);
+            this.buttonHast.TabIndex = 20;
+            this.buttonHast.Text = "Hash Verification";
+            this.buttonHast.Click += new System.EventHandler(this.ButtonHast_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(752, 444);
+            this.ClientSize = new System.Drawing.Size(1128, 683);
+            this.Controls.Add(this.buttonHast);
             this.Controls.Add(this.resetButton);
             this.Controls.Add(this.decryptButton);
             this.Controls.Add(this.label3);
@@ -355,7 +340,6 @@
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "ImageConverter";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -406,6 +390,7 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button decryptButton;
         private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Button buttonHast;
     }
 }
 
